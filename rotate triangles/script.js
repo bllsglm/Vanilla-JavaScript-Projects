cc31d8745fba7074c6535ac02e90492a31237a9b
bllsglm <bilalsaglam92@gmail.com>
2023-12-24 21:35:12 +0300

    rotate triangles

---
+++ rotate triangles/script.js
@@ -0,0 +1,17 @@
+// const kinetic = document.querySelector('.kinetic')
+// console.log(kinetic);
+
+
+// let degree = 0;
+// function rotateCube() {
+
+//   kinetic.style.rotate = `${degree}deg`
+  
+// }
+
+
+
+// setInterval(()=>{
+//   degree += 5;
+//   rotateCube()
+// },0.001)
